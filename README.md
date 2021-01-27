8f6b8125cc2ea9269823fa5ce20542b22f18559c
mic0 <59290502+mic0ishere@users.noreply.github.com>
2021-01-27 17:02:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Copy Raw Message
+
+Remake of DevilBro's BD [Copy Raw Message](https://github.com/mwittrien/BetterDiscordAddons/tree/master/Plugins/CopyRawMessage) plugin. ~~I copied some code from [Juby210](https://github.com/Juby210/view-raw)~~
---
+++ index.js
@@ -0,0 +1,44 @@
+const { Plugin } = require("powercord/entities");
+const { findInReactTree } = require("powercord/util");
+const { getModule, React } = require("powercord/webpack");
+const { inject, uninject } = require("powercord/injector");
+
+const CopyRawButton = require("./CopyRawButton");
+
+module.exports = class ViewRaw extends (
+	Plugin
+) {
+	startPlugin() {
+		this.loadStylesheet("style.css");
+
+		this.addButtons();
+	}
+
+	pluginWillUnload() {
+		this.addButtons(true, true);
+		document
+			.querySelectorAll(".copy-raw-message")
+			.forEach((e) => (e.style.display = "none"));
+	}
+
+	async addButtons(repatch, unpatch) {
+		if (repatch) {
+			uninject("copy-raw-message");
+		}
+		if (unpatch) return;
+		const MiniPopover = await getModule(
+			(m) => m?.default?.displayName === "MiniPopover"
+		);
+		inject("copy-raw-message", MiniPopover, "default", (_, res) => {
+			const props = findInReactTree(res, (r) => r?.message);
+			if (!props || (props.message.content.length < 1 && props.message.attachments.length < 1)) return res;
+			res.props.children.unshift(
+				React.createElement(CopyRawButton, {
+					message: props.message,
+				})
+			);
+			return res;
+		});
+		MiniPopover.default.displayName = "MiniPopover";
+	}
+};
